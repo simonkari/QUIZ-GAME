a15c02a08c1641ba2a60f5e11880e4f6396146eb
simonkari <kariithisimon1715@gmail.com>
2023-11-02 19:11:04 +0300

    quiz game

---
+++ data.py
@@ -0,0 +1,10 @@
+import requests
+
+parameters = {
+    "amount": 20,
+    "type": "boolean",
+    "category": 18
+}
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+question_data = response.json()["results"]
