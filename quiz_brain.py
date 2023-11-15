adbd78870cc721d6ff12427557036d00a5cdcfce
simonkari <kariithisimon1715@gmail.com>
2023-11-15 19:56:15 +0300

    chore:update questions

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,9 +1,11 @@
 import html
 
 
+# Class definition for the QuizBrain
 class QuizBrain:
 
     def __init__(self, q_list):
+        # Constructor method to initialize the QuizBrain object
         self.question_number = 0
         self.score = 0
         self.response = ""
@@ -11,9 +13,11 @@ class QuizBrain:
         self.current_question = None
 
     def still_has_questions(self):
+        # Method to check if there are still questions remaining
         return self.question_number < len(self.question_list)
 
     def next_question(self):
+        # Method to get the next question in the quiz
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
@@ -22,6 +26,7 @@ class QuizBrain:
         # self.check_answer(user_answer)
 
     def check_answer(self, user_answer):
+        # Method to check the user's answer against the correct answer
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
