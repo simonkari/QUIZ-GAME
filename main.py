28892dc0d04f095b09aa73e2c8422425b62f2574
simonkari <kariithisimon1715@gmail.com>
2023-11-15 19:50:43 +0300

    chore:update question

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from data import question_data
 from quiz_brain import QuizBrain
 from ui import QuizInterface
 
+# Create an empty list to store Question objects
 question_bank = []
+
+# Iterate through the question_data and create Question objects, then add them to the question_bank list
 for question in question_data:
     question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
+# Create a QuizBrain object with the populated question_bank list
 quiz = QuizBrain(question_bank)
+
+# Create a QuizInterface object, passing the quiz object for user interaction
 interface = QuizInterface(quiz)
